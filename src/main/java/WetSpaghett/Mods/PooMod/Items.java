d83ff4372e0f2d708cc06530fcb0439d26d5a1bd
WetSpaghett <likeabossjackie@gmail.com>
2021-12-30 16:08:00 +0000

    poo sword added

--- src/main/java/WetSpaghett/Mods/PooMod/Items.java
+++ src/main/java/WetSpaghett/Mods/PooMod/Items.java
@@ -1,10 +1,15 @@
 package WetSpaghett.Mods.PooMod;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
 
 @ObjectHolder(PooMod.MODID)
 public class Items {
     public static final Item poo = null;
     public static final Item pee = null;
+    public static final ItemSword poosword = null;
+    public static Item.ToolMaterial POO = EnumHelper.addToolMaterial("POO", 3, 512, 10f, 4f, 32);
 }
--- src/main/java/WetSpaghett/Mods/PooMod/RegistrationHandler.java
+++ src/main/java/WetSpaghett/Mods/PooMod/RegistrationHandler.java
@@ -1,8 +1,9 @@
 package WetSpaghett.Mods.PooMod;
 
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemSword;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,8 +13,9 @@ public class RegistrationHandler {
     @SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		final Item[] items = {
-				new Item().setRegistryName(PooMod.MODID, "poo").setUnlocalizedName(PooMod.MODID + "." + "poo").setCreativeTab(CreativeTabs.DECORATIONS),
-				new Item().setRegistryName(PooMod.MODID, "pee").setUnlocalizedName(PooMod.MODID + "." + "pee").setCreativeTab(CreativeTabs.DECORATIONS)
+				new ItemFood(8, 10f, true).setAlwaysEdible().setRegistryName(PooMod.MODID, "poo").setUnlocalizedName(PooMod.MODID + "." + "poo").setCreativeTab(CreativeTabs.FOOD),
+				new ItemFood(4, 10f, true).setAlwaysEdible().setRegistryName(PooMod.MODID, "pee").setUnlocalizedName(PooMod.MODID + "." + "pee").setCreativeTab(CreativeTabs.FOOD),
+				new ItemSword(Items.POO).setRegistryName(PooMod.MODID, "poosword").setUnlocalizedName(PooMod.MODID + "." + "poosword").setCreativeTab(CreativeTabs.COMBAT)
 		};
 		event.getRegistry().registerAll(items);
 	}
