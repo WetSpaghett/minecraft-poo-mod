0135c5ec3bca51ca28f28fabbe9f3f2cc9a398e4
WetSpaghett <likeabossjackie@gmail.com>
2022-01-13 08:26:32 +0000

    Actually change version number this time

--- src/main/java/WetSpaghett/Mods/PooMod/PooMod.java
+++ src/main/java/WetSpaghett/Mods/PooMod/PooMod.java
@@ -28,7 +28,7 @@ import static slimeknights.tconstruct.library.TinkerRegistry.*;
 public class PooMod {
     public static final String MODID = "poomod";
     public static final String NAME = "Poo Mod";
-    public static final String VERSION = "0.2.4";
+    public static final String VERSION = "0.3.0";
     public static final Logger LOGGER = LogManager.getLogger(PooMod.MODID);
     public static Item.ToolMaterial POOTOOL = EnumHelper.addToolMaterial("PooMod.POO", 3, 512, 10f, 4f, 32);
 
